a31f8fcd858a77f5005b7d3dcb49dac268406cdb
Sandeep kumar <sandeepk@geekyants.com>
2023-03-09 13:39:32 +0530

    chore: add environment variables

--- todo-app-auth-mail/backend/engine/src/server.js
+++ todo-app-auth-mail/backend/engine/src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const cronTab = require('./cron');
 const job = require('./queue/job');
 
