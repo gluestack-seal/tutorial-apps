a75412a5efcc2160cb25298655676024ab38ad31
Shivam Singhal <shivams@geekyants.com>
2023-03-10 13:47:36 +0530

    Env changes

--- todo-app-storage/todo-web/pages/index.tsx
+++ todo-app-storage/todo-web/pages/index.tsx
@@ -5,11 +5,11 @@ import InputBox from "../components/InputBox";
 import TodosContainer from "../components/TodosContainer";
 import TodosFooter from "../components/TodosFooter";
 
-const Home: NextPage = () => {
+const Home = ({NEXT_APP_NAME}: {NEXT_APP_NAME: string}) => {
   return (
     <div className="flex min-h-screen flex-col items-center mt-40 py-2 m-0 p-0">
       <Head>
-        <title>Todo App with Storage</title>
+        <title>{NEXT_APP_NAME}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -29,3 +29,11 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      NEXT_APP_NAME: process.env.NEXT_APP_NAME || "",
+    },
+  };
+}
